5f60992f5e8ae3b62c426c2fd8bb6b56ced05589
OceanNuclear <annoyingamygdala@gmail.com>
2022-01-25 16:35:05 +0000

    Made initial changes

--- inflammation/views.py
+++ inflammation/views.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-def visualize(data_dict):
+def visualize(data_dict, title=None):
     """Display plots of basic statistical properties of the inflammation data.
 
     :param data_dict: Dictionary of name -> data to plot
@@ -12,14 +12,14 @@ def visualize(data_dict):
     # TODO(lesson-design) Extend to allow saving figure to file
 
     num_plots = len(data_dict)
-    fig = plt.figure(figsize=((3 * num_plots) + 1, 3.0))
+    fig = plt.figure( figsize=(3.0, 3.0))
+    axes = fig.add_subplot()
 
-    for i, (name, data) in enumerate(data_dict.items()):
-        axes = fig.add_subplot(1, num_plots, i + 1)
-
-        axes.set_ylabel(name)
-        axes.plot(data)
-
+    for name, data in data_dict.items():
+        axes.plot(data, label=name)
+    axes.legend()
+    if title:
+        plt.title(title)
     fig.tight_layout()
-
+    
     plt.show()
