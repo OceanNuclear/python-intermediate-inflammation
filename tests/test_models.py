370bc2f436de0879a169e9fafe652a960b19ecd6
OceanNuclear <annoyingamygdala@gmail.com>
2022-01-26 16:45:32 +0000

    Made my first CI from scratch!

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,6 +4,18 @@ import pytest
 import numpy as np
 import numpy.testing as npt
 
+def test_load_csv_wrong_type():
+    """Test that load csv file only works with strings"""
+    from inflammation.models import load_csv
+
+    with pytest.raises(ValueError):
+        error_expected = load_csv(11111) # use a number in place of a str, should raise TypeError
+
+def test_load_csv_notAFile():
+    from inflamation.models import load_csv
+    with  pytest.raises(FileNotFoundError):
+        error_expected = load_csv("ARandomFileThatDefinitelyDoesntExistHere.csv")
+
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
     from inflammation.models import daily_mean
@@ -27,6 +39,13 @@ def test_daily_mean_integers():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
+
+def test_daily_mean_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_mean
+
+    with pytest.raises(TypeError):
+        error_expected = daily_mean([['Hello', 'there'], ['General', 'Kenobi']])
 
 
 def test_daily_min_zeros():
@@ -85,5 +104,13 @@ def test_daily_max_integers():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_max(test_input), test_result)
+
+def test_daily_max_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_max
+
+    with pytest.raises(TypeError):
+        error_expected = daily_max([['Hello', 'there'], ['General', 'Kenobi']])
+
 
 # TODO(lesson-robust) Implement tests for the other statistical functions
